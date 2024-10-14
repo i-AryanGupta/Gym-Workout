0270946a5d3f506abbf66a43e3f00e813a294fda
i-AryanGupta <aryangupta6008@gmail.com>
2024-10-14 12:55:55 +0530

    Initial Commit

---
+++ Gym Workout/Gym_WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_WorkoutApp.swift
+//  Gym Workout
+//
+//  Created by Yashom on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Gym_WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
